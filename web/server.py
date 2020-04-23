284796492a1e0f262abdc851015a78c668497035
Angello599 <angello@LAPTOP-1F3VD1MK.localdomain>
2020-04-23 16:15:10 -0500

    xd

--- web/server.py
+++ web/server.py
@@ -29,6 +29,37 @@ def static_content(content):
     return render_template(content)
 
 
+@app.route('/create_user/<nombre>/<apellido>/<passwd>/<uname>')
+def create_user(nombre, apellido, passwd, uname):
+    #crear un objeto (instancia de una entidad)
+    user = entities.User(
+        name = nombre,
+        fullname = apellido,
+        password = passwd,
+        username = uname
+    )
+
+    #Guardar el objeto en la capa de persistencia
+    db_session = db.getSession(engine)
+    db_session.add(user)
+    db_session.commit()
+
+    return "User created!"
+
+
+@app.route('/read_users')
+def read_users():
+    db_session = db.getSession(engine)
+    respuesta = db_session.query(entities.User)
+    users = respuesta[:]
+    i = 0
+    respuesta2= ""
+    for user in users:
+        respuesta2 += "Nombre: "+str(user.name)+ "&nbsp;&nbsp;&nbsp;&nbsp;"  +"Apellido: "+str(user.fullname) +"<br>"
+        print(i, "NAME:\t", user.name, "\t\t", "APELLIDO:\t", user.fullname)
+        i+=1
+    return respuesta2
+
 
 
 if __name__ == '__main__':
